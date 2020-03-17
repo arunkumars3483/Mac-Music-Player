7d3c6734985188eb341d1461a2d7785d5ce1b5fc
Arun <arunks@qburst.com>
2020-03-17 18:28:47 +0530

    added project files

---
+++ Malayalam Music/ViewController.swift
@@ -0,0 +1,160 @@
+//
+//  ViewController.swift
+//  Malayalam Music
+//
+//  Created by Arun kumar on 17/03/20.
+//  Copyright © 2020 qburst. All rights reserved.
+//
+
+import Cocoa
+import AVFoundation
+
+class ViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
+    
+    @IBOutlet weak var playerProgress: NSProgressIndicator!
+    @IBOutlet weak var playButton: NSButton!
+    @IBOutlet weak var stopButton: NSButton!
+    @IBOutlet weak var pauseButton: NSButton!
+    @IBOutlet weak var playerSlider: NSSlider!
+    
+    @IBOutlet weak var musicTableView: NSTableView!
+    @IBOutlet weak var musicName: NSTextField!
+    @IBOutlet weak var nowTime: NSTextField!
+    @IBOutlet weak var endTime: NSTextField!
+    
+    
+    
+    var player: AVPlayer?
+    var timeObserver: Any?
+    var currentIndex = 0
+    var musicfiles:[Dictionary<String, Any>] = []
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let path = Bundle.main.path(forResource: "musicdata", ofType: "json") {
+            do {
+                  let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                  let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+                  if let jsonResult = jsonResult as? Dictionary<String, AnyObject>, let music = jsonResult["files"] as? [Any] {
+                    musicfiles = music as? [Dictionary] ?? []
+                    print(musicfiles[0]["album"] ?? "")
+                  }
+              } catch {
+              }
+        }
+        
+        musicTableView.delegate = self
+        musicTableView.dataSource = self
+        
+        musicTableView.doubleAction = #selector(handleDoubleClick)
+    }
+    
+    override func viewWillAppear() {
+        playMusic(url: "https://mallusongsdownload.info" + (self.musicfiles[currentIndex]["url"] as! String))
+    }
+    
+    func playMusic(url: String){
+        let url = URL(string: url)
+        print(url)
+        let playerItem:AVPlayerItem = AVPlayerItem(url: url!)
+        player = AVPlayer(playerItem: playerItem)
+        musicName.stringValue = (self.musicfiles[currentIndex]["name"] as! String) + " (" + (self.musicfiles[currentIndex]["album"] as! String) + ")"
+        NotificationCenter.default.addObserver(self, selector: #selector(playerDidFinishPlaying), name: .AVPlayerItemDidPlayToEndTime, object: nil)
+        let interval = CMTime(seconds: 0.05, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+        timeObserver = player?.addPeriodicTimeObserver(forInterval: interval, queue: DispatchQueue.main, using: { elapsedTime in
+            self.updateSlider(elapsedTime: elapsedTime)
+        })
+    }
+    
+    @objc func playerDidFinishPlaying() {
+        if (currentIndex + 1) != self.musicfiles.count{
+            currentIndex = currentIndex + 1
+            playMusic(url: "https://mallusongsdownload.info" + (self.musicfiles[currentIndex]["url"] as! String))
+            player!.play()
+        }
+    }
+    
+    @objc func handleDoubleClick() {
+        let clickedRow = musicTableView.clickedRow
+        currentIndex = clickedRow
+        playMusic(url: "https://mallusongsdownload.info" + (self.musicfiles[clickedRow]["url"] as! String))
+        player!.play()
+    }
+    
+    func updateSlider(elapsedTime: CMTime) {
+        let playerDuration = playerItemDuration()
+        if CMTIME_IS_INVALID(playerDuration) {
+            playerSlider.minValue = 0.0
+            return
+        }
+        
+        let duration = Float(CMTimeGetSeconds(playerDuration))
+        if duration.isFinite && duration > 0 {
+            
+            let now = Float(CMTimeGetSeconds(elapsedTime))
+            nowTime.stringValue = String(  Int((Int(now) % 3600) / 60)  ) + ":" + String( ( Int( (Int(now) % 3600) ) % 60) )
+            endTime.stringValue = String(  Int((Int(duration) % 3600) / 60)  ) + ":" +  String( ( Int( (Int(duration) % 3600) ) % 60) )
+            playerSlider.minValue = 0.0
+            playerSlider.maxValue = Double(duration)
+            let time = Float(CMTimeGetSeconds(elapsedTime))
+            playerSlider.doubleValue = Double(time)
+        }
+    }
+    
+    private func playerItemDuration() -> CMTime {
+        let thePlayerItem = player?.currentItem
+        if thePlayerItem?.status == .readyToPlay {
+            return thePlayerItem!.duration
+        }
+        return .invalid
+    }
+    
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    
+    
+    @IBAction func playButtonClicked(_ sender: Any) {
+        
+        if player?.rate == 0
+        {
+            player!.play()
+            
+        } else {
+            player!.pause()
+            
+        }
+    }
+    
+    @IBAction func pauseButtonClicked(_ sender: Any) {
+        player!.pause()
+    }
+    
+    @IBAction func stopButtonClicked(_ sender: Any) {
+        player!.pause()
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "musicCell"), owner: nil) as? MusicCellView {
+          
+            let music = self.musicfiles[row]
+            
+            cell.musicLabel.stringValue = (music["name"] as! String).replacingOccurrences(of: ".ws.mp3", with: "")
+            cell.albumLabel.stringValue = (music["album"] as! String) + " (" + (music["year"] as! String) + ")"
+            
+          return cell
+        }
+        return nil
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        self.musicfiles.count
+    }
+    
+    
+    
+}
+
---
+++ Malayalam MusicTests/Malayalam_MusicTests.swift
@@ -0,0 +1,34 @@
+//
+//  Malayalam_MusicTests.swift
+//  Malayalam MusicTests
+//
+//  Created by Arun kumar on 17/03/20.
+//  Copyright © 2020 qburst. All rights reserved.
+//
+
+import XCTest
+@testable import Malayalam_Music
+
+class Malayalam_MusicTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
