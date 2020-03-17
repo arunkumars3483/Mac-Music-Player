e3b58a11df5353c67caa6819c568c7fcd2a1fda3
Arun <arunks@qburst.com>
2020-03-17 18:28:07 +0530

    Initial commit

---
+++ Malayalam Music/MusicCellView.swift
@@ -0,0 +1,24 @@
+//
+//  MusicCellView.swift
+//  Malayalam Music
+//
+//  Created by Arun kumar on 17/03/20.
+//  Copyright © 2020 qburst. All rights reserved.
+//
+
+import Cocoa
+
+class MusicCellView: NSTableCellView {
+
+    
+    @IBOutlet weak var musicLabel: NSTextField!
+    @IBOutlet weak var albumLabel: NSTextField!
+    
+    
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
